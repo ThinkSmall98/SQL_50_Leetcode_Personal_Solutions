4f3a5c506bf03b979fde032c1abbb15982f667ba
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-01-30 14:42:53 -0500

    Create consecutive_available_seats.sql

---
+++ sql50/consecutive_available_seats.sql
@@ -0,0 +1,8 @@
+-- self join & ABS
+SELECT 
+    DISTINCT C1.seat_id
+FROM Cinema C1
+INNER JOIN Cinema C2
+    ON ABS(C1.seat_id - C2.seat_id) = 1
+        AND C1.free = 1 AND C2.free = 1
+ORDER BY C1.seat_id ASC;
